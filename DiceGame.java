0d0415559d400507aa3cdead8def5e9386206b5b
fioriecheyenne <fioriecheyenne@gmail.com>
2019-11-14 16:42:21 -0700

    read me updated and code added

---
+++ DiceGame.java
@@ -0,0 +1,88 @@
+
+import java.util.Scanner;
+import java.text.NumberFormat;
+
+public class arenafight {
+        
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        //Ask user how many faces die 2 should have 
+        System.out.println("How many sides should the second die have?");
+
+        //Storing user response in die2 variable
+        int die2 = input.nextInt();
+
+        //Ask user how many times to roll the dice 
+        
+         System.out.println("How many times should we roll the dice?");
+
+        //Store response in rollCount variable
+        int rollCount = input.nextInt();
+
+        //Ask user how often to display results 
+         System.out.println("How often should we print results?");
+
+         //Store response in results variable 
+        int results = input.nextInt();
+
+        //Flush lingering eol character 
+        input.nextLine(); 
+
+        //Get users name 
+         System.out.println("What is your name?");
+
+        //Users name variable 
+         String uName = input.nextLine();
+
+        //Display experiment by user name, only displaying the first 5 characters of name 
+         String cutString = uName.substring(0,5);
+        System.out.println("\nExperiment by: " + cutString);
+
+        //Calling default constructor 
+            Die dice = new Die();
+            // System.out.println(dice.getNumSides());
+
+        //Calling paramaterized constructor 
+            Die dice2 = new Die(die2);
+            // System.out.println(dice2.getNumSides());
+        
+        //Roll counter variable 
+        int counter = 0; 
+        float total = 0; 
+       
+
+        //While loop to continue rolling dice until user designated rolls is met 
+        while (counter <= rollCount) {
+            dice.roll();
+            dice2.roll();
+            counter++;
+
+        //Creating sum variable to calculate dice sum
+            int sum = dice.roll() + dice2.roll(); 
+
+         //Conditions for how much money is won or lost 
+
+         //Sum less than 5,  user wins $3
+            if (sum < 5) {
+                total += 3;
+                }
+        //Sum equals 5, user wins $8
+             else if (sum == 5) {
+                 total += 8;
+                 }
+        //sum greater than 5, user loses $2
+             else if (sum > 5) {
+                total -= 2;
+                 }
+
+        //Display results at designated rolls 
+             if (counter % results == 0 ) {
+                 //Convert total winnings to proper currency formatting 
+                 NumberFormat defaultFormat = NumberFormat.getCurrencyInstance();
+                 System.out.println("Rolls: " + counter +"   Average winning per roll: " + defaultFormat.format(total / counter));
+             }
+}
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # DiceGame
-First time coding in Java - A dice game 
+
+First time coding in Java - A dice game
+
+The game has two die. One die has 6 sides, while the user determines how many sides the other has.
+The user is given $3 if the total is less than 5 and $8 if the total is equal to 5. If the total is more than 5, the user loses \$2.
+
+First, the user is asked a series of questions and the responses are stored.
+
+1. How many sides should the second die have
+2. How many times should the die be rolled
+3. How often should the results be printed
+4. What is the users name
+
+Then, the dice are then rolled and the results are printed.
